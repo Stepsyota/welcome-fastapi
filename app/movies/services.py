2b33863f8c772c2ab1a55843f321ae2e7e47a542
Stepan <stepsyota@gmail.com>
2026-01-14 12:46:05 +0300

    ADD: functions to new endpoints

--- app/movies/services.py
+++ app/movies/services.py
@@ -1,12 +1,63 @@
 import asyncio
 import random
 import time
+from http.client import HTTPException
+
 from app.movies.db import dict_movies
+from app.movies.schemas import MovieCreate, MovieUpdate, MovieReplace
 
 
 async def get_movie_from_db(id : int):
     await asyncio.sleep(0.2 + (random.random() / 10))
     return dict_movies.get(id)
+
+def len_db():
+    return len(dict_movies)
+
+
+async def set_movie_to_db(model : MovieCreate):
+    await asyncio.sleep(0.2 + (random.random() / 10))
+    id = len_db() + 1
+    dict_movie = {"movie" : model.movie, "year" : model.year}
+    dict_movies[id] = dict_movie
+    return id, dict_movie
+
+async def delete_movie_from_db(id : int):
+    await asyncio.sleep(0.2 + (random.random() / 10))
+    dict_movie = dict_movies.pop(id)
+    if dict_movie is None:
+        raise HTTPException(status_code=404, detail="Movie not found")
+    return dict_movie
+
+async def update_movie_from_db(id : int, model : MovieReplace):
+    await asyncio.sleep(0.2 + (random.random() / 10))
+    dict_movie = {"movie": model.movie, "year": model.year}
+    dict_movies[id] = dict_movie
+    return dict_movie
+
+async def update_part_movie_from_db(id : int, model : MovieUpdate):
+    await asyncio.sleep(0.2 + (random.random() / 10))
+
+    dict_movie = dict_movies.get(id)
+    if dict_movie is None:
+        raise HTTPException(status_code=404, detail="Movie not found")
+
+    if model.movie is not None:
+        dict_movie["movie"] = model.movie
+    if model.year is not None:
+        dict_movie["year"] = model.year
+
+    dict_movies[id] = dict_movie
+    return dict_movie
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# DAY 1. Just for experiments
+#
+# | | | | | | | | | | | | | | | | | | | |
+# v v v v v v v v v v v v v v v v v v v v
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 def sync_get_movie_from_db(id : int):
     time.sleep(0.2 + (random.random() / 10))
